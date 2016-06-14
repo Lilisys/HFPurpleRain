13c0a48a4cfe63a75996d95ce3ace7e7b916263c
mike <mike.swierenga@gmail.com>
2016-06-13 21:22:15 -0400

    made start for server

---
+++ node/server.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+var PDFParser = require("pdf2json/PDFParser");
+
+var outputCount = 0;
+
+app.get('/scrape', function(req, res){
+
+  //All the web scraping magic will happen here
+  searchGoogle();
+
+
+})
+
+app.listen('8081')
+
+console.log('Server running on port 8081');
+
+exports = module.exports = app;
+
+searchGoogle();
+
+
+function searchGoogle() {
+	var url = 'https://www.google.com/search?q=net+neutrality&as_filetype=pdf&num=25'
+	request(url, function(error, response, html) {
+
+  	if (!error)
+  	{
+  		var $ = cheerio.load(html);
+
+        var count = 0;
+        
+        $('.r').filter(function(){
+        	var r = $(this);
+
+        	var entry = r["0"]["children"];
+
+    		count++;
+
+        	for (var i = 0; i < entry.length; i++) {
+        		var a = entry[i]["attribs"]["href"];
+        		console.log(count, a.substring(7,a.length));
+        		performRequestOnGoogleResult(a.substring(7,a.length));
+        	}
+        })
+  	}
+
+  })
+}
+
+function performRequestOnGoogleResult(url) {
+	request(url, function(error, response, html) {
+
+
+		if (!error)
+		{
+
+		    var pdfParser = new PDFParser();
+
+		    pdfParser.on("pdfParser_dataError", errData => console.error("error: ", errData.parserError) );
+		    pdfParser.on("pdfParser_dataReady", pdfData => {
+		    	console.log(getRawTextContent());
+		        fs.writeFile(outputCount + ".json", JSON.stringify(pdfData));
+		        outputCount++;
+		    });
+
+		    pdfParser.loadPDF("./fulltext.pdf");
+		}
+
+	})
+
+}
